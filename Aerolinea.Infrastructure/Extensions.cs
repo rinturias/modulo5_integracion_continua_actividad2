28d9480fe033ada1412a7737caee26aa87a288ca
Ronal Inturias <ronal.95.03@gmail.com>
2022-07-29 00:01:45 -0400

    prueba

--- Aerolinea.Infrastructure/Extensions.cs
+++ Aerolinea.Infrastructure/Extensions.cs
@@ -21,9 +21,6 @@ namespace Aerolinea.Vuelos.Infrastructure {
 
             services.AddDbContext<ReadDbContext>(context => context.UseSqlServer(connectionString));
             services.AddDbContext<WriteDbContext>(context => context.UseSqlServer(connectionString));
-            //using (var scop= services.) {
-
-            //}
 
 
             services.AddScoped<IVueloRepository, VueloRepository>();
